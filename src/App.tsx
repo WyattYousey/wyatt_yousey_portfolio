3b94a3dc33520e37076d36d6267edf3a05ae82d5
wyattYousey <quiz.nv1@gmail.com>
2026-05-30 00:08:06 -0500

    v0.1 Initial Scaffold for project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">Wyatt Portfolio</h1>
+    </div>
+  );
+}
+
+export default App
