639a790ecd64198fbedd6abbbbc957e637a58317
wyattYousey <quiz.nv1@gmail.com>
2026-05-30 14:54:58 -0500

    v.0.3.2 Modify skill data shape to better fit what we require, also implement currentWorkflow to prepare for all programatically rendered items on page.

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,3 +1,71 @@
-export const skills = {
-    core: ['JavaScript', 'React', 'Node.js', 'Java', 'Python', 'MongoDB', 'Git'],
-};
+import javascriptIcon from '../assets/javascript-logo.svg';
+import reactIcon from '../assets/react-logo.svg';
+import nodeIcon from '../assets/node-js-logo.svg';
+import javaIcon from '../assets/java-logo.svg';
+import pythonIcon from '../assets/python-logo.svg';
+import mongoIcon from '../assets/mongodb-logo.svg';
+import gitIcon from '../assets/git-logo.svg';
+
+export interface Skill {
+  icon: string;
+  title: string;
+}
+
+export const skills: Skill[] = [
+  {
+    icon: javascriptIcon,
+    title: 'JavaScript',
+  },
+  {
+    icon: reactIcon,
+    title: 'React',
+  },
+  {
+    icon: nodeIcon,
+    title: 'Node.js',
+  },
+  {
+    icon: javaIcon,
+    title: 'Java',
+  },
+  {
+    icon: pythonIcon,
+    title: 'Python',
+  },
+  {
+    icon: mongoIcon,
+    title: 'MongoDB',
+  },
+  {
+    icon: gitIcon,
+    title: 'Git',
+  },
+];
+
+export interface CurrentWorkflow {
+    item: string;
+    accent: string;
+}
+
+export const currentWorkflow: CurrentWorkflow[] = [
+    {
+        item: 'Research',
+        accent: '(--accent-1)'
+    },
+    {
+        item: 'Plan',
+        accent: '(--accent-2)'
+    },
+    {
+        item: 'Build',
+        accent: '(--accent-3)'
+    },
+    {
+        item: 'Refine',
+        accent: '(--accent-4)'
+    },
+    {
+        item: 'Optimize',
+        accent: '(--accent-5)'
+    }
+]
