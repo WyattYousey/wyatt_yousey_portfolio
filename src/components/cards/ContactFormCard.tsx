66d8461b16e1e642577dffe5d02985040f75422e
wyattYousey <quiz.nv1@gmail.com>
2026-06-01 15:22:01 -0500

    v0.5.4 Implement Framer Motion animations on the form and introduced new animation objects to simplify component (move to external file later in codebase managment phase)

--- src/components/cards/ContactFormCard.tsx
+++ src/components/cards/ContactFormCard.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
+import { motion, type Variants } from 'framer-motion';
 
 interface Inputs {
   nameInput: string;
@@ -12,6 +13,47 @@ interface FormData {
   email: string;
   message: string;
 }
+
+const containerVariants: Variants = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.4,
+    },
+  },
+};
+
+const slideFromLeft: Variants = {
+  hidden: {
+    opacity: 0,
+    x: -850,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 100,
+      damping: 15,
+    },
+  },
+};
+
+const slideFromRight: Variants = {
+  hidden: {
+    opacity: 0,
+    x: 850,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 100,
+      damping: 15,
+    },
+  },
+};
 
 const ContactFormCard = ({ nameInput, emailInput, messageInput }: Inputs) => {
   const [formData, setFormData] = useState<FormData>({
@@ -33,6 +75,14 @@ const ContactFormCard = ({ nameInput, emailInput, messageInput }: Inputs) => {
   const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsSending(true);
+
+    if (
+      formData.name === '' ||
+      formData.email === '' ||
+      formData.message === ''
+    ) {
+      return;
+    }
 
     try {
       await emailjs.send(
@@ -60,45 +110,90 @@ const ContactFormCard = ({ nameInput, emailInput, messageInput }: Inputs) => {
   };
 
   return (
-    <form id="contact-form" onSubmit={handleSubmit}>
-      <label htmlFor="name">
+    <motion.form
+      id="contact-form"
+      onSubmit={handleSubmit}
+      className="flex flex-col m-auto mt-10 gap-4 text-xl w-fit text-(--color-text)"
+      variants={containerVariants}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+    >
+      <motion.label
+        variants={slideFromLeft}
+        htmlFor="name"
+        className=" flex gap-4 bg-(--color-card-bg) p-3 rounded-t-2xl"
+      >
         {nameInput}
         <input
+          className="w-300 focus:outline-1 focus:outline-black rounded-md pl-2 text-(--color-text) focus:bg-(--color-button-bg)"
           type="text"
-          name="name"
+          name="nameData"
           id="name"
           value={formData.name}
           onChange={handleChange}
           minLength={2}
           maxLength={46}
+          autoComplete="name"
+          required
         />
-      </label>
-      <label htmlFor="email">
+      </motion.label>
+      <motion.label
+        variants={slideFromRight}
+        htmlFor="email"
+        className="flex gap-4 bg-(--color-card-bg) p-3"
+      >
         {emailInput}
         <input
+          className="w-300 focus:outline-1 focus:outline-black rounded-md pl-2 text-(--color-text) focus:bg-(--color-button-bg)"
           type="email"
           value={formData.email}
           onChange={handleChange}
-          name="email"
+          name="emailData"
           id="email"
+          autoComplete="email"
+          required
         />
-      </label>
-      <label htmlFor="message">
+      </motion.label>
+      <motion.label
+        variants={slideFromLeft}
+        htmlFor="message"
+        className="bg-(--color-card-bg) flex relative gap-4 p-3 rounded-b-2xl"
+      >
         {messageInput}
         <textarea
-          name="message"
+          className="w-275 max-h-35 focus:outline-1 focus:outline-black rounded-md pl-2 text-(--color-text) focus:bg-(--color-button-bg)"
+          name="messageData"
+          id="message"
           value={formData.message}
           onChange={handleChange}
           minLength={2}
+          autoComplete="off"
+          required
         />
-        <button
-          className="px-4 py-2 text-(--color-text) font-bold text-2xl border border-(--color-text) rounded-md bg-(--color-button-bg) hover:text-(--color-button-bg-hover) hover:text-3xl hover:cursor-pointer hover:text-shadow-(--shadow-button-hover) transition-all duration-450 ease-in-out"
+        <motion.button
+          initial={{
+            opacity: 0,
+            scale: 0.8,
+          }}
+          whileInView={{
+            opacity: 1,
+            scale: 1,
+          }}
+          viewport={{ once: true }}
+          transition={{
+            delay: 1.5,
+            type: 'spring',
+            stiffness: 200,
+            damping: 10,
+          }}
+          className="absolute bottom-2 right-5 px-4 py-2 text-(--color-text) font-bold text-2xl border border-(--color-text) rounded-md bg-(--color-button-bg) hover:text-(--color-button-bg-hover) hover:text-3xl hover:cursor-pointer hover:text-shadow-(--shadow-button-hover) transition-all duration-450 ease-in-out"
           type="submit"
         >
           {isSending ? 'Sending...' : 'Send'}
-        </button>
-      </label>
-    </form>
+        </motion.button>
+      </motion.label>
+    </motion.form>
   );
 };
 
