9486e85642c4951e655ecb59191746e8023b4b1a
wyattYousey <quiz.nv1@gmail.com>
2026-06-01 11:18:39 -0500

    v0.4.6 Implement new button hover styles where the words turn dark, get larger,  and a text shadow applies that is white, giving a neat effect.

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,20 @@
+interface Button {
+    text: string;
+    link: string;
+}
+
+const Button = ({ text, link }: Button) => {
+  return (
+    <button
+      className="px-4 py-2  mt-2 text-(--color-text) font-bold text-2xl border border-(--color-text) rounded-md bg-(--color-button-bg) hover:text-(--color-button-bg-hover) hover:text-3xl hover:cursor-pointer hover:text-shadow-(--shadow-button-hover) transition-all duration-450 ease-in-out"
+      onClick={() => {
+        window.location.href = link;
+      }}
+      type="button"
+    >
+      {text}
+    </button>
+  );
+}
+
+export default Button
