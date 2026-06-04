87df08dfc139a7ca5df513dcea3f96eb1d7b3bd5
wyattYousey <quiz.nv1@gmail.com>
2026-06-04 10:32:05 -0500

    v1.1.1 Implement screenBreaks Object to localize any screenBreak information and prep for responsive implementation

---
+++ src/data/screenBreaks.ts
@@ -0,0 +1,19 @@
+interface ScreenBreak {
+    largeDesktop: number;
+    mediumDesktop: number;
+    smallDesktop: number;
+    largeTablet: number;
+    smallTablet: number;
+    largePhone: number;
+    smallPhone: number;
+}
+
+export const screenBreakMap: ScreenBreak = {
+    largeDesktop: 1775,
+    mediumDesktop: 1585,
+    smallDesktop: 1246,
+    largeTablet: 980,
+    smallTablet: 755,
+    largePhone: 565,
+    smallPhone: 380
+}
