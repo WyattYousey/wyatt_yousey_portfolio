428429b5c02d7b570ffc84c58fb88b94d499f414
wyattYousey <quiz.nv1@gmail.com>
2026-06-04 10:35:32 -0500

    v1.2.4 Adjust main layout to be more responsive to different screen sizes

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/navigation/Navbar';
+import { screenBreakMap } from '../data/screenBreaks';
 
 export function MainLayout() {
+  const screenWidth = window.innerWidth;
+
   return (
-    <div className="bg-(--color-page-bg) h-screen overflow-hidden">
+    <div className={`bg-(--color-page-bg) ${screenWidth > screenBreakMap.largeDesktop ? 'h-screen overflow-hidden' : 'h-fit'}`}>
       <Navbar />
       <main>
         <Outlet />
