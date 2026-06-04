7812f46ae3ed824f5a219c419a51794cd8d8cd22
wyattYousey <quiz.nv1@gmail.com>
2026-06-04 10:33:27 -0500

    v1.2.1 Implement responsive design for Experience Page

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,20 +1,22 @@
 import { Fragment } from 'react/jsx-runtime';
-import { ArrowRight } from 'lucide-react';
+import { ArrowDown, ArrowRight } from 'lucide-react';
 import { currentlyExploringItems, stats } from '../data/experience';
 import { timeline } from '../data/experience';
 import StatCard from '../components/cards/StatCard';
 import TimelineCard from '../components/cards/TimelineCard';
 import PillCard from '../components/cards/PillCard';
 import { motion } from 'framer-motion';
+import { screenBreakMap } from '../data/screenBreaks';
 
 const Experience = () => {
+  const screenWidth = window.innerWidth;
   return (
-    <div className="p-6">
+    <div className={`p-6 ${screenWidth < screenBreakMap.largePhone ? 'mt-20' : 'mt-5'}`}>
       <div>
         <h2 className="typography-heading text-(--color-text) ml-10 mt-10">
           Experience Stats
         </h2>
-        <div className="flex gap-15 items-center justify-center mt-5">
+        <div className={`${ screenWidth < screenBreakMap.largePhone ? "grid grid-cols-2 gap-2 place-items-center" : 'flex gap-15 items-center justify-center'} mt-5`}>
           {stats.map((stat) => (
             <StatCard key={stat.caption} {...stat} />
           ))}
@@ -24,7 +26,7 @@ const Experience = () => {
         <h3 className="typography-heading text-(--color-text) ml-10 mt-5">
           Experience Timeline
         </h3>
-        <div className="flex gap-10 items-center justify-center mt-15">
+        <div className={`flex ${screenWidth < screenBreakMap.smallDesktop ? 'flex-col' : ''} gap-10 items-center justify-center mt-15`}>
           {timeline.map((card, index) => (
             <Fragment key={card.title}>
               <motion.div
@@ -41,7 +43,7 @@ const Experience = () => {
               </motion.div>
 
               {index < timeline.length - 1 && (
-                <ArrowRight size={150} className="text-(--color-button-bg)" />
+                screenWidth < screenBreakMap.smallDesktop ? <ArrowDown size={50} className="text-(--color-button-bg)" /> : <ArrowRight size={150} className="text-(--color-button-bg)" />
               )}
             </Fragment>
           ))}
@@ -51,7 +53,9 @@ const Experience = () => {
         <h4 className="typography-heading text-(--color-text) ml-10 mt-10">
           Currently Exploring
         </h4>
-        <div className="grid grid-cols-4 place-items-center mt-10 gap-8">
+        <div
+          className={`grid ${screenWidth < screenBreakMap.largePhone ? 'grid-cols-1' : screenWidth < screenBreakMap.mediumDesktop ? 'grid-cols-2' : 'grid-cols-4'} place-items-center mt-10 gap-8`}
+        >
           {currentlyExploringItems.map((item) => (
             <Fragment key={item.item}>
               <PillCard {...item} />
